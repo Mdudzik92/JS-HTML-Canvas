dfb6f9841c26bfe37ce18123aec230903cb2b12d
Matt Dudzik <mdudzik92@gmail.com>
2021-08-06 22:29:44 -0500

    Logging an event on mousedown

---
+++ script.js
@@ -0,0 +1,24 @@
+const canvas = document.querySelector("#draw");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.strokeStyle = "#COOL";
+ctx.lineJoin = "round";
+ctx.lineCap = "round";
+
+let isDrawing = false;
+// Where we start the line from
+let lastX = 0;
+// Where we end the line
+let lastY = 0;
+
+function draw(e) {
+	// Stops the function from running when they're not moused
+	if (!isDrawing) return;
+	console.log(e);
+}
+
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mousedown", () => (isDrawing = true));
+canvas.addEventListener("mouseup", () => (isDrawing = false));
+canvas.addEventListener("mouseout", () => (isDrawing = false));
