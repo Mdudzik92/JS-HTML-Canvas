a288385e8c59acd8ab08c777cd88853087c00cd8
Matt Dudzik <mdudzik92@gmail.com>
2021-08-06 22:55:10 -0500

    Updating the hue and width of the line direction

--- script.js
+++ script.js
@@ -12,11 +12,15 @@ let isDrawing = false;
 let lastX = 0;
 // Where we end the line
 let lastY = 0;
+let hue = 0;
+let direction = true;
 
 function draw(e) {
 	// Stops the function from running when they're not moused
 	if (!isDrawing) return;
 	console.log(e);
+	// Adding color, starting at red hue
+	ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
 	ctx.beginPath();
 	// Starting and ending our lines
 	ctx.moveTo(lastX, lastY);
@@ -24,6 +28,22 @@ function draw(e) {
 	ctx.stroke();
 	// Destructuring an array in one line with ES6
 	[lastX, lastY] = [e.offsetX, e.offsetY];
+
+	hue++;
+	if (hue >= 360) {
+		hue = 0;
+	}
+	// If the line width is greater than 100, reverse the direction of the width size
+	// If it's less than 1 reverse it again in the opposite direction
+	if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+		direction = !direction;
+	}
+	// Depending on what that direction is we either increment or decrement the line width
+	if (direction) {
+		ctx.lineWidth++;
+	} else {
+		ctx.lineWidth--;
+	}
 }
 
 // As soon as the user clicks the mouse down to start it, we update the lastX and lastY
